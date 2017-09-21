bc7573059abc72cea01735d551b90e16aef0349e
vitor cirilo <vitor.cirilo3@gmail.com>
2017-09-20 21:18:31 -0300

    update

--- app.R
+++ app.R
@@ -65,6 +65,32 @@ ui <- dashboardPage(
 )
 
 server <- function(input, output) {
+  observeEvent(input$uploadFilesBtn, {
+    
+    if(!is.null(input$uploadDataFiles)){
+      dataFiles <- input$uploadDataFiles %>% fixUploadedFilesNames
+      
+      list_files = list()
+      
+      for(i in 1:length(input$uploadDataFiles[,1])){
+        # file_name_input <- list_files[[i]]
+        # file_name_input2 <- list_2[i]
+        
+        Sys.sleep(0.1)
+        print(dataFiles$name[i])
+        
+        kaiju_taxonpath_input <- paste0(dataFiles$datapath[i])
+        
+        taxon <- read.delim2(kaiju_taxonpath_input, sep="\t", header = FALSE, stringsAsFactors=F)
+        
+        
+        
+        
+      }
+    }
+    
+  })
+  
   set.seed(122)
   histdata <- rnorm(500)
   
